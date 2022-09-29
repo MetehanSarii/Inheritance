af1187cbe6883deae3766e373d05a0eb7e9980c3
MetehanSarii <110286862+MetehanSarii@users.noreply.github.com>
2022-09-29 04:07:11 +0300

    Add files via upload

---
+++ İnheritance/src/CustomerManager.java
@@ -0,0 +1,10 @@
+
+public class CustomerManager {
+	public void List() {
+		System.out.println("Müþteri Listelendi");
+	}
+	public void add() {
+		System.out.println("Müþteri Eklendi");
+	}
+
+}
---
+++ İnheritance/src/EmployeeManager.java
@@ -0,0 +1,13 @@
+
+public class EmployeeManager {
+	public void List() {
+		System.out.println("Müþteri Listelendi");
+	}
+	public void add() {
+		System.out.println("Müþteri Eklendi");
+	}
+	public void bestEmployee() {
+		System.out.println("Ayýn Elamaný Getirildi");
+	}
+
+}
